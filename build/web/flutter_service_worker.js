0284596f304dd5dd985c87e5820171712e9188cd
Eye-Mans <laluiman468@gmail.com>
2024-12-09 14:44:04 +0700

    P Update!

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "3e795316de89cc2877877ab5aef6200f",
+const RESOURCES = {"flutter_bootstrap.js": "8c2cddf445c6c1fd47223c494b19b1eb",
 "version.json": "ddf124a409212666cb934be51032cb38",
 "index.html": "ae1b826a956c99a5e87c6c1b488f5e05",
 "/": "ae1b826a956c99a5e87c6c1b488f5e05",
-"main.dart.js": "9f81a79e26d722aaf75666b97e963e36",
+"main.dart.js": "473c45bf6446c27dd0af7e6e00e02d2b",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
